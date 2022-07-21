952c3d7ea28ba9f32b0b906f235e34d6bb66c07a
satrn8 <verypyc@gmail.com>
2022-07-21 22:56:36 +0300

    first commit

--- test_practice_form/test_student_registration_form.py
+++ test_practice_form/test_student_registration_form.py
@@ -7,19 +7,19 @@ from demoqa_tests.controls.tags_input import TagsInput
 from demoqa_tests.controls.dropdown import Dropdown
 
 
-class Months(Enum):
-    January = 0
-    February = 1
-    March = 2
-    April = 3
-    May = 4
-    June = 5
-    July = 6
-    August = 7
-    September = 8
-    October = 9
-    November = 10
-    December = 11
+# class Months(Enum):
+#     January = 0
+#     February = 1
+#     March = 2
+#     April = 3
+#     May = 4
+#     June = 5
+#     July = 6
+#     August = 7
+#     September = 8
+#     October = 9
+#     November = 10
+#     December = 11
 
 
 def open_form():
