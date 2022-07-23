9ff29b80f0b8b737337d2747c4e5b6749149c6dc
satrn8 <verypyc@gmail.com>
2022-07-23 11:50:57 +0300

    commit

---
+++ demoqa_tests/model/controls/table.py
@@ -0,0 +1,9 @@
+from selene.core.entity import Element
+
+
+class Table:
+    def __init__(self, element: Element):
+        self.element = element
+
+    def cells_of_row(self, index):
+        return self.element.all('tbody tr')[index].all('td')
